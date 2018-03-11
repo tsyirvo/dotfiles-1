cc9016e9c71f1ea242d6493c65686b9a2080ea74
Tsyirvo <gabriel.hofman.00@gmail.com>
2018-03-11 19:37:49 +0100

    Add Antigen setup

---
+++ antigen/config.zsh
@@ -0,0 +1,4 @@
+source $ZSH/antigen/antigen.zsh
+
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
---
+++ antigen/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# Installs antigen. 
+#
+curl -L git.io/antigen > $ZSH/antigen/antigen.zsh
