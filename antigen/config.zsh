f5038f9a186ed755801892d87b82737ef1bcfa73
Tsyirvo <gabriel.hofman.00@gmail.com>
2018-03-11 20:41:40 +0100

    Add some Antigen bundles

--- antigen/config.zsh
+++ antigen/config.zsh
@@ -1,4 +1,15 @@
 source $ZSH/antigen/antigen.zsh
 
+# Bundles from the default repo (robbyrussell's oh-my-zsh).
+antigen bundle last-working-dir
+# antigen bundle ssh-agent
+antigen bundle sudo
+antigen bundle rand-quote
+antigen bundle colored-man
+
+# Other bundles.
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
+
+# Theming
+# antigen theme ys
