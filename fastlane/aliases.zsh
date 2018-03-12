f99616384a8765e1c86b6e9288301263918c1cbb
Tsyirvo <gabriel.hofman.00@gmail.com>
2018-03-12 16:55:37 +0100

    Add Fastlane aliases

---
+++ fastlane/aliases.zsh
@@ -0,0 +1,8 @@
+# Fastlane Match
+alias fmd='fastlane match development'
+alias fma='fastlane match app-store'
+alias fmr='fastlane match --readonly'
+
+# Fastlane releases
+alias fbf='fastlane betaFabric'
+alias fbt='fastlane betaTestFlight'
