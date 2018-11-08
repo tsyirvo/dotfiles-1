c763cd954d45f04abbbce8c95c32edab1bdcb707
Tsyirvo <gabriel.hofman.00@gmail.com>
2018-11-08 15:21:55 +0100

    Add a git alias for stash && clear

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -26,3 +26,4 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
 alias greb='git checkout develop && git pull origin develop && git checkout - && git rebase develop'
+alias gsc='git stash && git stash clear'
